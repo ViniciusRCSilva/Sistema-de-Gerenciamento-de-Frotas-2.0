7aa2456f834a67198a67587dd2db6df2324216f4
davi-souza2001 <davisouza2001dv@gmail.com>
2022-11-28 11:45:36 -0300

    up

--- pages/tabela.tsx
+++ pages/tabela.tsx
@@ -1,9 +1,24 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Header  from '../components/HeaderTabela'
+import Header from '../components/HeaderTabela'
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Tabela: NextPage = () => {
+  const [lista, setLista] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:3001').then((list) => {
+      setLista(list.data.dados)
+    })
+
+  }, [])
+
+  lista.map((dado) => {
+    console.log(dado)
+  })
+
   return (
     <div className='flex min-h-screen flex-col bg-[#00aad4ff]'>
       <Header />
@@ -22,67 +37,54 @@ const Tabela: NextPage = () => {
 
         <table className="w-[70%] text-center shadow-xl">
 
-            <thead className="border-b bg-gradient-to-r from-[#056f92] to-[#063954] text-white">
-              <tr>
-                <th scope="col" className="text-sm font-medium text-white px-6 py-4">
-                  ID
-                </th>
-                <th scope="col" className="text-sm font-medium text-white px-6 py-4">
-                  Data de coleta de dados
-                </th>
-                <th scope="col" className="text-sm font-medium text-white px-6 py-4">
-                  Bluetooth
-                </th>
-                <th scope="col" className="text-sm font-medium text-white px-6 py-4">
-                  Carro
-                </th>
-                <th scope="col" className="text-sm font-medium text-white px-6 py-4">
-                  Obd Conectado
-                </th>
-              </tr>
+          <thead className="border-b bg-gradient-to-r from-[#056f92] to-[#063954] text-white">
+            <tr>
+              <th scope="col" className="text-sm font-medium text-white px-6 py-4">
+                ID
+              </th>
+              <th scope="col" className="text-sm font-medium text-white px-6 py-4">
+                Data de coleta de dados
+              </th>
+              <th scope="col" className="text-sm font-medium text-white px-6 py-4">
+                Bluetooth
+              </th>
+              <th scope="col" className="text-sm font-medium text-white px-6 py-4">
+                Carro
+              </th>
+              <th scope="col" className="text-sm font-medium text-white px-6 py-4">
+                Obd Conectado
+              </th>
+            </tr>
 
-            </thead>
+          </thead>
 
-            <tbody>
+          <tbody>
 
-              <tr className="bg-white border-b">
-                <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                  1
-                </td>
-                <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                  26/10/2022 21:07:37
-                </td>
-                <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                  Conectado
-                </td>
-                <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                  FUSCA
-                </td>
-                <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                  Conectado
-                </td>
-              </tr>
+            {lista.map((dado: any) => {
+              return (
+                <tr className="bg-white border-b">
+                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                    {dado.id}
+                  </td>
+                  <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                    26/10/2022 21:07:37
+                    {dado.collectTimestamp}
+                  </td>
+                  <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                    {dado.bluetoothIsEnable ? 'Conectado' : 'Desconectado'}
+                  </td>
+                  <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                    {dado.carDescription}
+                  </td>
+                  <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                    {dado.connectObd ? 'Conectado' : 'Desconectado'}
+                  </td>
+                </tr>
+              )
+            })}
 
-              <tr className="bg-white border-b">
-                <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                  2
-                </td>
-                <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                  26/10/2022 21:38:50
-                </td>
-                <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                  Conectado
-                </td>
-                <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                  FERRARI
-                </td>
-                <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                  Conectado
-                </td>
-              </tr>
-
-            </tbody>
-          </table>
+          </tbody>
+        </table>
       </div>
     </div>
   )
